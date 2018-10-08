95cce750656b334ef199dc1b5f20e3250bc1b9a0
Andrew Pinkham <code@andrewsforge.com>
2018-10-08 11:23:08 -0400

    🚀 Clean up setup.py; remove run_reqs

--- setup.py
+++ setup.py
@@ -1,75 +1,61 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-==================================
-Markdown Subscript Extension Setup
-==================================
+"""Markdown Subscript Extension Setup
 
 :website: https://github.com/jambonrose/markdown_subscript_extension
-:copyright: Copyright 2014-2017 Andrew Pinkham
+:copyright: Copyright 2014-2018 Andrew Pinkham
 :license: Simplified BSD, see LICENSE for details.
 """
 
-from setuptools import setup
 from codecs import open as codec_open
 from os import path
 
+from setuptools import setup
 
 HERE = path.abspath(path.dirname(__file__))
 
 
 # Get the long description from the Read Me
-with codec_open(path.join(HERE, 'README.rst'), encoding='utf-8') as f:
+with codec_open(path.join(HERE, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
-# Get runtime dependencies
-run_reqs = 'requirements/run_requirements.txt'
-with codec_open(path.join(HERE, run_reqs), encoding='utf-8') as f:
-    install_requires = f.read().splitlines()
-
 # Get test dependencies
-test_reqs = 'requirements/test_requirements.txt'
-with codec_open(path.join(HERE, test_reqs), encoding='utf-8') as f:
+test_reqs = "requirements/test_requirements.txt"
+with codec_open(path.join(HERE, test_reqs), encoding="utf-8") as f:
     tests_require = f.read().splitlines()
 
 
 setup(
-    name='MarkdownSubscript',
-
-    version='2.1.0',  # PEP 440 Compliant Semantic Versioning
-
-    keywords=['text', 'filter', 'markdown', 'html', 'subscript'],
-    description='Python-Markdown extension to allow for subscript text.',
+    name="MarkdownSubscript",
+    version="2.1.0",  # PEP 440 Compliant Semantic Versioning
+    keywords=["text", "filter", "markdown", "html", "subscript"],
+    description="Python-Markdown extension to allow for subscript text.",
     long_description=long_description,
-
-    author='Andrew Pinkham',
-    author_email='hello at andrewsforge dot com',
-
-    url='https://github.com/jambonrose/markdown_subscript_extension',
-
-    py_modules=['mdx_subscript'],
-    install_requires=install_requires,
-    test_suite='nose.collector',
+    author="Andrew Pinkham",
+    url="https://github.com/jambonrose/markdown_subscript_extension",
+    py_modules=["mdx_subscript"],
+    install_requires=["Markdown>=2.5,<3.1"],
+    test_suite="nose.collector",
     tests_require=tests_require,
-
-    license='Simplified BSD License',
+    license="Simplified BSD License",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Text Processing :: Filters',
-        'Topic :: Text Processing :: Markup :: HTML',
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Filters",
+        "Topic :: Text Processing :: Markup :: HTML",
     ],
 )
